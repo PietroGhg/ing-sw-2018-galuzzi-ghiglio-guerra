8b6f84041a5e33e82fb62e023bcfe60a35cb75de
leoguerra8 <leonardo1.guerra@mail.polimi.it>
2018-05-05 19:07:58 +0200

    Commit 9 (3): Implementazione ObjectiveCard Public e Private

--- src/main/java/it/polimi/se2018/Model/ObjectiveCards/PublicObjectiveCard/RowColourVariety.java
+++ src/main/java/it/polimi/se2018/Model/ObjectiveCards/PublicObjectiveCard/RowColourVariety.java
@@ -6,6 +6,24 @@ public class RowColourVariety extends PublicObjectiveCard { //Colori diversi - R
     /* Rows with no repeated colours */
     @Override
     public int getScore(WPC wpc){
-        return 0;
+        int score=0;
+        boolean allDifferent=true;
+        /*
+        For each row, check if there are at least two dice with the same
+        colour: if at least one row has all the dice of different colours
+        the resultant score is 6, otherwise the score is 0
+        */
+        for(int j=0; j<WPC.NUMROW; j++){
+            allDifferent=true;
+            for(int i=0; i<(WPC.NUMCOL)-1; i++){
+                /* fino a NUMCOL-1 perchè il confronto va fatto fino al penultimo elemento con l'ultimo */
+                for(int z=i+1; z<WPC.NUMROW; z++){
+                    if(wpc.getCell(i,j).getDie().getDieColour().equals(wpc.getCell(i,z).getDie().getDieColour()))
+                        allDifferent=false;
+                }
+            }
+            if(allDifferent==true) score=6;
+        }
+        return score;
     }
 }
--- src/main/java/it/polimi/se2018/Model/ObjectiveCards/PublicObjectiveCard/RowShadeVariety.java
+++ src/main/java/it/polimi/se2018/Model/ObjectiveCards/PublicObjectiveCard/RowShadeVariety.java
@@ -6,6 +6,24 @@ public class RowShadeVariety extends PublicObjectiveCard {  //Sfumature diverse 
     /* Rows with no repeated values */
     @Override
     public int getScore(WPC wpc){
-        return 0;
+        int score=0;
+        boolean allDifferent=true;
+        /*
+        For each row, check if there are at least two dice with the same
+        value: if at least one row has all the dice of different values
+        the resultant score is 5, otherwise the score is 0
+        */
+        for(int j=0; j<WPC.NUMROW; j++){
+            allDifferent=true;
+            for(int i=0; i<(WPC.NUMCOL)-1; i++){
+                /* fino a NUMCOL-1 perchè il confronto va fatto fino al penultimo elemento con l'ultimo */
+                for(int z=i+1; z<WPC.NUMROW; z++){
+                    if(wpc.getCell(i,j).getDie().getDieValue()==wpc.getCell(i,z).getDie().getDieValue())
+                        allDifferent=false;
+                }
+            }
+            if(allDifferent==true) score=5;
+        }
+        return score;
     }
 }
