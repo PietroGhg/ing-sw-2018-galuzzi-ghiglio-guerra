22edaaa5e7e93a23aa7e8f587711f7890e5c447b
PietroGhg <38155419+PietroGhg@users.noreply.github.com>
2018-05-08 10:50:12 +0200

    Commit 12: classe dicebag

--- src/main/java/it/polimi/se2018/Model/Die.java
+++ src/main/java/it/polimi/se2018/Model/Die.java
@@ -1,12 +1,30 @@
 package it.polimi.se2018.Model;
 
+import java.util.Random;
+
 public class Die {
     private Integer value;
     private Colour colour;
 
+    public Die(int v, String c){
+        value = Integer.valueOf(v);
+        colour = Colour.valueOf(c);
+    }
+
+    public Die(int v, Colour c){
+        value = Integer.valueOf(v);
+        colour = c;
+    }
+
+    public Die(Colour c){
+        colour = c;
+    }
+
     public Integer getDieValue() { return value; }
     public Colour getDieColour() { return colour; }
-    public void roll(){
 
+    public void extract(){
+        Random rand = new Random();
+        value = rand.nextInt(6) + 1; //generates a number between 0 and 5 and adds 1 [1;6]
     }
 }
--- src/main/java/it/polimi/se2018/Model/Table/DiceBag.java
+++ src/main/java/it/polimi/se2018/Model/Table/DiceBag.java
@@ -6,10 +6,32 @@ import it.polimi.se2018.Model.Die;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class DiceBag {
-    private int diceTotal;
-    private Map<Colour, Integer> diceColour = new HashMap<>();
-    /*public ArrayList<Die> extractDice(){
-    }*/
+    private ArrayList<Die> bag;
+    private final int DICETOTAL = 90;
+
+    //instanzia i 90 dadi
+    public DiceBag(){
+        bag = new ArrayList<>();
+        for(Colour c : Colour.values()){
+            for(int i = 0; i < DICETOTAL/Colour.values().length; i++){
+                bag.add(new Die(c));
+            }
+        }
+    }
+
+    public ArrayList<Die> extractDice(int nPlayers){
+        ArrayList<Die> result = new ArrayList<>();
+        Random rand = new Random();
+        for(int i = 0; i < nPlayers + 1; i++){
+            int index = rand.nextInt(bag.size());
+            Die die = bag.get(index);
+            die.extract();
+            result.add(die);
+            bag.remove(index);
+        }
+        return result;
+    }
 }
