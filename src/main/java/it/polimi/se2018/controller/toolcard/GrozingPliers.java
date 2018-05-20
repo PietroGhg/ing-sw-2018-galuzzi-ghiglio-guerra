19a07411ca681cbddb4e6a6d88c402d9f46a8683
leoguerra8 <leonardo1.guerra@mail.polimi.it>
2018-05-20 13:31:55 +0200

    Commit 35: modifiche a ToolCard, RestrictionChecker, e GrozingPliers

--- src/main/java/it/polimi/se2018/controller/toolcard/GrozingPliers.java
+++ src/main/java/it/polimi/se2018/controller/toolcard/GrozingPliers.java
@@ -12,9 +12,21 @@ import it.polimi.se2018.model.wpc.WPC;
 
 public class GrozingPliers implements ToolCard{   //Pinza Sgrossatrice
 
+    private static GrozingPliers instance;
+    private GrozingPliers(){};
+    public static GrozingPliers getInstance(){
+        if (instance==null) instance = new GrozingPliers();
+        return instance;
+    }
+
+    private int favorTokensNeeded=1;
+    public int getFavorTokensNeeded(){ return favorTokensNeeded; }
+
     public void cardAction(PlayerMoveParameters param) throws MoveNotAllowedException{
-        WPC temp = new WPC( param.getWpc() );
         RestrictionChecker rc = new RestrictionChecker();
+        rc.checkEnoughFavorTokens(param.getPlayer(),instance);
+
+        WPC temp = new WPC( param.getWpc() );
 
         int row = param.getParameter(0);
         int col = param.getParameter(1);
@@ -25,5 +37,12 @@ public class GrozingPliers implements ToolCard{   //Pinza Sgrossatrice
         if (increment == -1) temp.getCell(row, col).getDie().decrease();
 
         param.getPlayer().setWpc(temp);
+
+        int currentFT = param.getPlayer().getFavorTokens() - favorTokensNeeded;
+        param.getPlayer().setFavorTokens(currentFT);
+
+        if (favorTokensNeeded == 1){
+            favorTokensNeeded = 2;
+        }
     }
 }
