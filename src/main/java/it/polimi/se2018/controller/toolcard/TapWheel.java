1bf6bb61482743912d971cc8c9e541de13a687d8
leoguerra8 <leonardo1.guerra@mail.polimi.it>
2018-05-23 19:39:10 +0200

    Commit 46: modifiche a RestrictionChecker e ToolCard

--- src/main/java/it/polimi/se2018/controller/toolcard/TapWheel.java
+++ src/main/java/it/polimi/se2018/controller/toolcard/TapWheel.java
@@ -1,8 +1,20 @@
 package it.polimi.se2018.controller.toolcard;
 
+import it.polimi.se2018.controller.RestrictionChecker;
 import it.polimi.se2018.exceptions.MoveNotAllowedException;
+import it.polimi.se2018.model.Colour;
+import it.polimi.se2018.model.Die;
+import it.polimi.se2018.model.Player;
 import it.polimi.se2018.model.PlayerMoveParameters;
 import it.polimi.se2018.model.table.Model;
+import it.polimi.se2018.model.wpc.WPC;
+
+import java.util.ArrayList;
+
+/**
+ * Class for ToolCard TapWheel
+ * @author Leonardo Guerra
+ */
 
 public class TapWheel implements ToolCard{    //Taglierina Manuale
 
@@ -17,7 +29,57 @@ public class TapWheel implements ToolCard{    //Taglierina Manuale
     public int getFavorTokensNeeded(){ return favorTokensNeeded; }
 
     @Override
-    public void cardAction(Model model) {
+    public void cardAction(Model model) throws MoveNotAllowedException{
+        RestrictionChecker rc = new RestrictionChecker();
+        PlayerMoveParameters param = model.getParameters();
+        int playerID = param.getPlayerID();
+        Player player = model.getPlayer(playerID);
+        rc.checkEnoughFavorTokens(player,instance);
 
+        WPC wpc = player.getWpc();
+
+        int rtNumber = param.getParameter(0);
+        int rtIndex = param.getParameter(1);
+        int rowDie1 = param.getParameter(2);
+        int colDie1 = param.getParameter(3);
+        int rowCell1 = param.getParameter(4);
+        int colCell1 = param.getParameter(5);
+
+        ArrayList<ArrayList<Die>> rt = model.getRoundTrack().getRT();
+        rc.checkRTCellNotEmpty(rt,rtNumber,rtIndex);
+        rc.checkMatchingColour(wpc.getCell(rowDie1,colDie1).getDie(),rt.get(rtNumber).get(rtIndex));
+        Die d1 = new Die(wpc.getCell(rowDie1, colDie1).getDie().getDieValue(), wpc.getCell(rowDie1, colDie1).getDie().getDieColour());
+        rc.checkAdjacent(wpc,rowCell1,colCell1);
+        rc.checkEmptiness(wpc,rowCell1,colCell1);
+        rc.checkSameDie(wpc,rowCell1,colCell1,d1);
+        rc.checkValueRestriction(wpc,rowCell1,colCell1,d1);
+        rc.checkColourRestriction(wpc,rowCell1,colCell1,d1);
+        wpc.setDie(rowCell1,colCell1,d1);
+        wpc.removeDie(rowDie1,colDie1);
+
+        //Second die moving
+        if(model.paramCount(param)==10){
+            int rowDie2 = param.getParameter(6);
+            int colDie2 = param.getParameter(7);
+            int rowCell2 = param.getParameter(8);
+            int colCell2 = param.getParameter(9);
+            rc.checkMatchingColour(wpc.getCell(rowDie2,colDie2).getDie(),rt.get(rtNumber).get(rtIndex));
+            Die d2 = new Die(wpc.getCell(rowDie2, colDie2).getDie().getDieValue(), wpc.getCell(rowDie2, colDie2).getDie().getDieColour());
+            rc.checkAdjacent(wpc,rowCell2,colCell2);
+            rc.checkEmptiness(wpc,rowCell2,colCell2);
+            rc.checkSameDie(wpc,rowCell2,colCell2,d2);
+            rc.checkValueRestriction(wpc,rowCell2,colCell2,d2);
+            rc.checkColourRestriction(wpc,rowCell2,colCell2,d2);
+            wpc.setDie(rowCell2,colCell2,d2);
+            wpc.removeDie(rowDie2,colDie2);
+            //spostare un altro dado
+        }
+
+        int currentFT = player.getFavorTokens() - favorTokensNeeded;
+        player.setFavorTokens(currentFT);
+
+        if (favorTokensNeeded == 1){
+            favorTokensNeeded = 2;
+        }
     }
 }
