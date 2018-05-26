1e63b539599c543e7a0801f94e53bef540a5fe03
PietroGhg <38155419+PietroGhg@users.noreply.github.com>
2018-05-26 17:09:04 +0200

    Implementazione metodi run() e send() delle connessioni

--- src/main/java/it/polimi/se2018/networking/client/SocketServerConnection.java
+++ src/main/java/it/polimi/se2018/networking/client/SocketServerConnection.java
@@ -1,23 +1,62 @@
 package it.polimi.se2018.networking.client;
 
 import it.polimi.se2018.controller.VCAbstractMessage;
+import it.polimi.se2018.view.MVAbstractMessage;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+/**
+ * Implementation of the ServerConnection class
+ * @author Pietro Ghiglio
+ */
 public class SocketServerConnection extends ServerConnection {
     private Socket socket;
 
+    private ObjectOutputStream objectOutputStream;
+
+    private ObjectInputStream objectInputStream;
+
     public SocketServerConnection(Socket socket){
         this.socket = socket;
+        try{
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
+    //synchronized ??
     public void send(VCAbstractMessage message){
         //sends the message
+        try {
+            objectOutputStream.writeObject(message);
+            objectOutputStream.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void run(){
         //deserilizes MVAbstractMessages and notifies them
+        boolean loop = true;
+        MVAbstractMessage message;
+
+        while(loop){
+            try{
+                message = (MVAbstractMessage)objectInputStream.readObject();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void update(VCAbstractMessage message){
--- src/main/java/it/polimi/se2018/networking/server/SocketClientConnection.java
+++ src/main/java/it/polimi/se2018/networking/server/SocketClientConnection.java
@@ -1,25 +1,63 @@
 package it.polimi.se2018.networking.server;
 
+import it.polimi.se2018.controller.VCAbstractMessage;
 import it.polimi.se2018.view.MVAbstractMessage;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
  * Implementation of the ClientConnection class
+ * @author Pietro Ghiglio
  */
 public class SocketClientConnection extends ClientConnection {
     private Socket socket;
 
+    private ObjectInputStream objectInputStream;
+
+    private ObjectOutputStream objectOutputStream;
+
     public SocketClientConnection(Socket socket){
+
         this.socket = socket;
+        try{
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void run(){
+    public void run() {
         //deserializes VCAbstractMessages and notifies them
+        VCAbstractMessage message;
+        boolean loop = true;
+
+        try {
+            while (loop) {
+                message = (VCAbstractMessage) objectInputStream.readObject();
+                notify(message);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            loop = false;
+            //PlayerDisconnectedException ??
+        }
     }
 
+    //Synchronized???
     public void send(MVAbstractMessage message){
         //send the message
+        try {
+            objectOutputStream.writeObject(message);
+            objectOutputStream.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
