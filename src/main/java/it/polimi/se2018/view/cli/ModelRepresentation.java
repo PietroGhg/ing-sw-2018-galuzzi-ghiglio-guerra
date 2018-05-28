f3840caa099956c19a34196a5e59c5cd616e7896
Okami <andrea.galuzzi@mail.polimi.it>
2018-05-28 13:35:32 +0200

    View: ModelRepreentation, metodo visit per MVSetUpMessage, MVGameMessage e Extracted Card Message,  visualizzazione scelta WPC.

---
+++ src/main/java/it/polimi/se2018/view/cli/ModelRepresentation.java
@@ -0,0 +1,56 @@
+package it.polimi.se2018.view.cli;
+
+import java.util.Map;
+
+public class ModelRepresentation {
+    private String message;
+    private String roundTrack;
+    private String draftPool;
+
+
+    private String prCards;
+    private String [] puCards;
+
+    private Map<Integer, String> wpcs;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getRoundTrack() {
+        return roundTrack;
+    }
+
+    public String getDraftPool() {
+        return draftPool;
+    }
+
+    public Map<Integer, String> getWpcs() {
+        return wpcs;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setRoundTrack(String roundTrack) {
+        this.roundTrack = roundTrack;
+    }
+
+    public void setDraftPool(String draftPool) {
+        this.draftPool = draftPool;
+    }
+
+    public void setWpcs(int playerID, String wpc) {
+        wpcs.put(playerID, wpc);
+    }
+
+    public void setWpcs(Map<Integer, String> wpcs) {
+        this.wpcs = wpcs;
+    }
+
+    public void setPrCards(String prCards) { this.prCards = prCards; }
+
+    //hpublic void setPuCards(String puCards) { this.puCards = puCards; }
+
+}
