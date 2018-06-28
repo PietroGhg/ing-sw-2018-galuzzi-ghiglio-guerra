baedd64e1544e7190ed0626d4170fdccf5e2f40d
leoguerra8 <leonardo1.guerra@mail.polimi.it>
2018-06-28 18:42:40 +0200

    Test per Model

--- src/test/java/it/polimi/se2018/TestModel.java
+++ src/test/java/it/polimi/se2018/TestModel.java
@@ -1,8 +1,12 @@
 package it.polimi.se2018;
 
+import it.polimi.se2018.exceptions.UserNameNotFoundException;
 import it.polimi.se2018.model.Player;
 import it.polimi.se2018.model.table.Model;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TestModel {
     private Model model;
@@ -10,16 +14,49 @@ public class TestModel {
     @Test
     public void test(){
         model = new Model();
-        Player player;
-        player = new Player(1);
+        Player player = new Player(1);
+        player.setDisconnected(false);
+        player.setSkipTurn(false);
         player.setDisconnected(true);
         player.setSkipTurn(true);
-        System.out.println(player.isDisconnected());
-        System.out.println(player.getSkipTurn());
         model.addPlayer("leo");
-        model.addPlayer(new Player(2));
-        model.addPlayer(new Player(3));
+        model.addPlayer("pietro");
+        model.addPlayer("andrea");
         model.startGame();
         model.nextTurn();
+        model.getDiscPlayers();
+        model.getPlayerNames();
+        model.addDiscPlayer("gio");
+        model.removeDiscPlayer("gio");
+        model.addPlayerName("gigi");
+        model.removePlayerName("gigi");
+        assertEquals(3, model.numActivePlayers());
+        try{
+            model.getPlayer("leo");
+        }
+        catch (UserNameNotFoundException e){
+            fail();
+        }
+        model.removePlayer("leo");
+        try{
+            model.getPlayer("leo");
+            fail();
+        }
+        catch (UserNameNotFoundException e){
+
+        }
+
+        model.setStartGameMessage("ciao", 1);
+        model.setWelcomeBackMessage(1,"leo","bentornato");
+        model.setWinnerMessage("leo");
+
+        model.setMVTimesUpMessage();
+        model.setWpc(1,1);
+        model.setDiscMessage("bye");
+        model.setReady(1);
+        assertEquals(false, model.allReady());
+        model.setReady(1);
+        model.setReady(2);
+        assertEquals(true, model.allReady());
     }
 }
