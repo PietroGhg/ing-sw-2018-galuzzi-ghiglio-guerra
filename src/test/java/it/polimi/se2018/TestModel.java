a8471fb404e3b6d07f649a2843b0df5de93a4da4
leoguerra8 <leonardo1.guerra@mail.polimi.it>
2018-06-29 23:42:09 +0200

    Test per Model

--- src/test/java/it/polimi/se2018/TestModel.java
+++ src/test/java/it/polimi/se2018/TestModel.java
@@ -1,5 +1,7 @@
 package it.polimi.se2018;
 
+import it.polimi.se2018.controller.turntimer.TurnTimer;
+import it.polimi.se2018.exceptions.GameEndedException;
 import it.polimi.se2018.exceptions.UserNameNotFoundException;
 import it.polimi.se2018.model.Player;
 import it.polimi.se2018.model.table.Model;
@@ -12,7 +14,7 @@ public class TestModel {
     private Model model;
 
     @Test
-    public void test(){
+    public void test() {
         model = new Model();
         Player player = new Player(1);
         player.setDisconnected(false);
@@ -24,45 +26,46 @@ public class TestModel {
         model.addPlayer("andrea");
         model.startGame();
         model.nextTurn();
-        model.getDiscPlayers();
         assertEquals(0, model.getDiscPlayers().size());
-        //TODO: controlla che sia uguale a [leo, pietro, andrea]
-        model.getPlayerNames();
-        //TODO: controlla che ci sia gio
+        assertEquals("leo", model.getPlayerNames().get(0));
+        assertEquals("pietro", model.getPlayerNames().get(1));
+        assertEquals("andrea", model.getPlayerNames().get(2));
         model.addDiscPlayer("gio");
+        assertEquals("gio", model.getDiscPlayers().get(0));
         model.removeDiscPlayer("gio");
         model.addPlayerName("gigi");
         model.removePlayerName("gigi");
         assertEquals(3, model.numActivePlayers());
-        try{
+        try {
             assertEquals("leo", model.getPlayer("leo").getName());
-        }
-        catch (UserNameNotFoundException e){
+        } catch (UserNameNotFoundException e) {
             fail();
         }
         model.removePlayer("leo");
-        try{
+        try {
             model.getPlayer("leo");
             fail();
-        }
-        catch (UserNameNotFoundException e){
-    
-        }
+        } catch (UserNameNotFoundException e) {
 
+        }
         model.setStartGameMessage("ciao", 1);
-        model.setWelcomeBackMessage(1,"leo","bentornato");
+        model.setWelcomeBackMessage(1, "leo", "bentornato");
         model.setWinnerMessage("leo");
-
         model.setMVTimesUpMessage();
-        model.setWpc(1,1);
-        assertEquals(1,model.getPlayer(1).getWpc().getId());
+        model.setWpc(1, 1);
+        assertEquals(1, model.getPlayer(1).getWpc().getId());
         model.setDiscMessage("bye");
         model.setReady(1);
-        //TODO: controlla che il giocatore 1 abbia flagReady a 1
-
+        assertEquals(true, model.getPlayer(1).isReady());
         assertEquals(false, model.allReady());
         model.setReady(1);
         model.setReady(2);
         assertEquals(true, model.allReady());
     }
 }
+
+/*  TEST
+    TODO: test WPC, Controller, Controller>TurnTimer, Controller>States
+    TODO: Controller>VCMessageCreator, Controller>VCMessageCreator>parametersgetter,
+    TODO: Controller>VCMessageCreator>parametersgetter>decorator,
+*/
